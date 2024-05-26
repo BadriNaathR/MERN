bf1bdabd869815afdf555071e3f0d59dfea91247
BadriNaathR <badrinaath145@gmail.com>
2024-05-26 13:58:54 +0530

    third update

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className= 'bg-blue font-primary'>Hi Soliders!!!!!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/layout/Main.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { Outlet } from 'react-router-dom'
+import Navbar from '../Compenents/Navbar'
+import '../App.css'
+import Footer from '../Compenents/footer'
+import { AuthContext } from '../context/AuthProvider'
+
+const Main = () => {
+  const {loading} = useContext(AuthContext);
+  return (
+    
+      loading ? <p>Loading........</p>:
+      <div>
+        <Navbar/>
+        <div className='min-h-screen'>
+          <Outlet/>
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Main
